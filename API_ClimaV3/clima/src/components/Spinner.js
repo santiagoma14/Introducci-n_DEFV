4e522e24b129f042458e7284b8afaa3ccc3e2f69
santiagoma14 <santiago.mayorga@outlook.es>
2023-08-18 09:51:14 -0500

    Subo proyecto de consumo de API

---
+++ API_ClimaV3/clima/src/components/Spinner.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../assets/css/Spinner.css";
+
+const Spinner = () =>{
+
+    return(
+        <div className="lds-hourglass">
+
+        </div>
+    );
+}
+
+export default Spinner;
